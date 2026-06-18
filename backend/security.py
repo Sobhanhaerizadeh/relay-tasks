c889fed50572c7d3b59dc1ff2a23e8c3f0ffcc5b
Mohammadhosein(Nima) Bahrami <bahraminima16@gmail.com>
2026-06-18 10:11:26 +0200

    sicheres Passwort-Hashing mit bcrypt implementiert

---
+++ backend/security.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def hash_password(password: str) -> str:
+    hashed_password = pwd_context.hash(password)
+    return hashed_password
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    result =pwd_context.verify(password,hashed_password)
+    return result
